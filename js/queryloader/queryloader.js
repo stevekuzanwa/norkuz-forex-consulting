68b14bd09884b0ff490654c74bf9e51d44b194be
Steve Kuzanwa <stevekuzanwa@outlook.com>
2019-08-13 21:03:25 +0200

    Norkuz Forex Consulting

---
+++ js/queryloader/queryloader.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function() {
+    new QueryLoader2(document.querySelector("body"), {
+        barColor: "#fff",
+        backgroundColor: "#F05422",
+        barHeight: 2,
+        minimumTime: 500,
+        fadeOutTime: 1000
+    });
+});
+
+
